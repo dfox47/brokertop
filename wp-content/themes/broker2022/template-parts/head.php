5bd13e4eb33cabc2ca95d3f0fe605f7ab8b23bc4
Dennis FOX <dfox@foxartbox.com>
2022-04-23 02:26:31 +0300

    Home slider styles. Part 5

--- wp-content/themes/broker2022/template-parts/head.php
+++ wp-content/themes/broker2022/template-parts/head.php
@@ -32,35 +32,63 @@
 		</header>
 	</div>
 
-	<div class="hero_block__slider">
-		<section class="js-splide-slider splide" data-splide='{"type":"loop","perPage":1}'>
-			<div class="splide__track">
-				<ul class="splide__list">
-					<li class="splide__slide">
-						<div class="hero_block_slide">
-							<div class="hero_block_slide__title">Bvlgari Hotel<br />Residences Moscow</div>
-
-							<div class="hero_block_slide__address">Большая Никитская ул., 9/15с1, Москва</div>
-
-							<div class="hero_block_slide__more">
-								<a class="btn btn_more" href="#">Подробнее</a>
-							</div>
-
-							<div class="hero_block_slide__developer">Capital towers</div>
-
-							<div class="hero_block_slide__img">img</div>
-						</div>
-					</li>
-
-					<li class="splide__slide">Slide 02</li>
-
-					<li class="splide__slide">Slide 03</li>
-				</ul>
-			</div>
-		</section>
-	</div>
-
 	<?php if (is_front_page()) { ?>
+		<div class="hero_block__slider">
+			<section class="js-splide-slider splide" data-splide='{"type":"loop","perPage":1}'>
+				<div class="splide__track">
+					<ul class="splide__list">
+						<li class="splide__slide">
+							<div class="hero_block_slide">
+								<div class="hero_block_slide__title">Bvlgari Hotel<br />Residences Moscow</div>
+
+								<div class="hero_block_slide__address">Большая Никитская ул., 9/15с1, Москва</div>
+
+								<div class="hero_block_slide__more">
+									<a class="btn btn_more" href="#">Подробнее</a>
+								</div>
+
+								<div class="hero_block_slide__developer">Capital towers</div>
+
+								<div class="hero_block_slide__img">img</div>
+							</div>
+						</li>
+
+						<li class="splide__slide">
+							<div class="hero_block_slide">
+								<div class="hero_block_slide__title">Bvlgari Hotel 2<br />Residences Moscow</div>
+
+								<div class="hero_block_slide__address">Большая Никитская ул., 9/15с1, Москва</div>
+
+								<div class="hero_block_slide__more">
+									<a class="btn btn_more" href="#">Подробнее</a>
+								</div>
+
+								<div class="hero_block_slide__developer">Capital towers</div>
+
+								<div class="hero_block_slide__img">img</div>
+							</div>
+						</li>
+
+						<li class="splide__slide">
+							<div class="hero_block_slide">
+								<div class="hero_block_slide__title">Bvlgari Hotel 3<br />Residences Moscow</div>
+
+								<div class="hero_block_slide__address">Большая Никитская ул., 9/15с1, Москва</div>
+
+								<div class="hero_block_slide__more">
+									<a class="btn btn_more" href="#">Подробнее</a>
+								</div>
+
+								<div class="hero_block_slide__developer">Capital towers</div>
+
+								<div class="hero_block_slide__img">img</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+
 		<div class="hero_block__footer">
 			<?php echo(date("Y")); ?> TOP BROKER ESTATE.<br />
 			Москва, Пресненская набережная 8 стр.1, 571
