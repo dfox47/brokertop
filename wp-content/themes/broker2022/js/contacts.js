f090ade27ec1efbee7e05435246202b65538573e
Dennis FOX <dfox@foxartbox.com>
2022-04-24 14:03:51 +0300

    Contact form logic.

--- wp-content/themes/broker2022/js/contacts.js
+++ wp-content/themes/broker2022/js/contacts.js
@@ -1,14 +1,16 @@
 
-// events for contact form 7
-let $contactInput = document.querySelectorAll('.wpcf7-form-control')
+// inputs [START]
+let $contactInputs = document.getElementsByClassName('wpcf7-form-control')
 
-$contactInput.forEach((input) => {
+Array.from($contactInputs).forEach(function(input) {
 	let $label      = input.closest('label')
 	let inputVal    = input.value
 
-	if ($label) {
-		$label.classList.toggle('active', inputVal)
+	if (!$label) {
+		return
 	}
+
+	$label.classList.toggle('active', inputVal)
 
 	input.addEventListener('input', (e) => {
 		$label.classList.toggle('active', e.target.value)
@@ -22,3 +24,18 @@ $contactInput.forEach((input) => {
 		$label.classList.add('active')
 	})
 })
+// inputs [END]
+
+// response [START]
+let $contactError = document.getElementsByClassName('wpcf7-response-output')
+
+Array.from($contactError).forEach(function(output) {
+	output.addEventListener('click', (e) => {
+		let $form = e.target.closest('.wpcf7-form')
+
+		$form.dataset.status = 'init'
+		$form.classList.remove('invalid')
+		$form.classList.add('init')
+	})
+})
+// response [END]
--- wp-content/themes/broker2022/js/header.js
+++ wp-content/themes/broker2022/js/header.js
@@ -1,9 +1,9 @@
 
 // header menu toggle
-let headerMenuToggle    = document.querySelectorAll('.js-header-menu-toggle')
+let $headerMenuToggle   = document.querySelectorAll('.js-header-menu-toggle')
 let $html               = document.querySelector('html')
 
-headerMenuToggle.forEach((button) => {
+$headerMenuToggle.forEach((button) => {
 	button.addEventListener('click', () => {
 		$html.classList.toggle('header_menu_active')
 	})
