967c5af351504d82966daa499ceff5257dc4cbeb
Dennis FOX <dfox@foxartbox.com>
2022-04-25 01:23:56 +0300

    Phone. Check for value exist.

--- wp-content/themes/broker2022/template-parts/phone.php
+++ wp-content/themes/broker2022/template-parts/phone.php
@@ -1,6 +1,9 @@
 
 <?php // all options https://brokertop.ru/wp-admin/admin.php?page=theme-custom-options
-$phone          = esc_attr(get_option('broker_phone'));
-$phoneShort     = preg_replace('/[()\s-]/', '', $phone); ?>
+$phone = esc_attr(get_option('broker_phone'));
 
-<a class="phone" href="tel:<?php echo $phoneShort; ?>"><?php echo $phone; ?></a>
+if ( isset($phone) ) {
+	$phoneShort = preg_replace('/[()\s-]/', '', $phone); ?>
+
+	<a class="phone" href="tel:<?php echo $phoneShort; ?>"><?php echo $phone; ?></a>
+<?php } ?>
