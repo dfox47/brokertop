e2ff53158acd87e2b1dfc8ebd8bd04a82ded7c60
Dennis FOX <dfox@foxartbox.com>
2022-05-02 22:44:49 +0300

    Single page optimise.

--- wp-content/themes/broker2022/single.php
+++ wp-content/themes/broker2022/single.php
@@ -3,7 +3,7 @@
 
 <main class="main_content_wrap">
 	<div class="main_content">
-		<div class="wrap2 zzzzzzzz">
+		<div class="wrap2">
 			<h1><?php single_post_title(); ?></h1>
 
 			<?php // content
