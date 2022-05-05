1475071e819b90110c95a604855344e4b20fb58a
Dennis FOX <dfox@foxartbox.com>
2022-05-05 16:48:36 +0300

    H1 updates. Single page.

--- wp-content/themes/broker2022/single.php
+++ wp-content/themes/broker2022/single.php
@@ -4,7 +4,9 @@
 <main class="main_content_wrap">
 	<div class="main_content">
 		<div class="wrap2">
-			<h1><?php single_post_title(); ?></h1>
+			<?php if (single_post_title()) { ?>
+				<h1><?php single_post_title(); ?></h1>
+			<?php } ?>
 
 			<?php // content
 			the_content(); ?>
