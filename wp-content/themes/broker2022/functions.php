a7a6ceaf2817bba3c8629ca91b2bd6f33080d770
Dennis FOX <dfox@foxartbox.com>
2022-05-18 20:32:46 +0300

    Product image to the background at category page. Part 2

--- wp-content/themes/broker2022/functions.php
+++ wp-content/themes/broker2022/functions.php
@@ -37,7 +37,7 @@ function woocommerceThemeSupport() {
 add_action('after_setup_theme', 'woocommerceThemeSupport');
 
 // pagination error fix
-function remove_page_from_query_string($query_string) {
+function removePageFromQueryString($query_string) {
 	if ($query_string['name'] == 'page' && isset($query_string['page'])) {
 		unset($query_string['name']);
 		$query_string['paged'] = $query_string['page'];
@@ -45,7 +45,7 @@ function remove_page_from_query_string($query_string) {
 
 	return $query_string;
 }
-add_filter('request', 'remove_page_from_query_string');
+add_filter('request', 'removePageFromQueryString');
 
 add_filter('woof_clear_all_text', function($default_text) {
 	return 'Сбросить фильтр';
@@ -77,3 +77,34 @@ function categoryPageProductAttributes() {
 	echo '<ul class="product_attributes">' . implode('', $attr_output) . '</ul>';
 }
 add_action('woocommerce_after_shop_loop_item_title', 'categoryPageProductAttributes');
+
+
+
+function templateLoopProductTitle() {
+	echo '<h2 class="product_cat__title">' . get_the_title() . '</h2>';
+}
+
+function switchLoopTitle() {
+	remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
+	add_action('woocommerce_after_shop_loop_item_title', 'templateLoopProductTitle', 0);
+}
+add_action('woocommerce_before_shop_loop_item', 'switchLoopTitle');
+
+
+
+// remove default product link at category page
+remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+
+function replaceProductLinkAtCategory() {
+	global $product;
+	$affiliate_link = get_post_meta(get_the_ID(), '_product_url', true);
+
+	if ($affiliate_link) {
+		echo '<a href="' . esc_url($affiliate_link) . '" class="product_link" target="_blank">';
+	}
+	else {
+		echo '<a href="' . get_the_permalink() . '" class="product_link" style="background-image: url(' . wp_get_attachment_url( $product->get_image_id() ) . ')"><span class="product_link__content">';
+	}
+}
+
+add_action('woocommerce_before_shop_loop_item', 'replaceProductLinkAtCategory', 10);
