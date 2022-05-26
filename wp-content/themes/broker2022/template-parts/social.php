7188376bcea7fdcd00a15e9354878bf4061a61d9
Dennis FOX <dfox@foxartbox.com>
2022-05-26 17:41:45 +0300

    Instagram link from admin options.

--- wp-content/themes/broker2022/template-parts/social.php
+++ wp-content/themes/broker2022/template-parts/social.php
@@ -8,7 +8,12 @@
 		<a class="social_list__icon social_list__icon--telegram" href="" target="_blank"></a>
 	</li>
 
-	<li class="social_list__item">
-		<a class="social_list__icon social_list__icon--instagram" href="" target="_blank"></a>
-	</li>
+	<?php // all options https://brokertop.ru/wp-admin/admin.php?page=theme-custom-options
+	$instagram = esc_attr(get_option('broker_inst'));
+
+	if ( isset($instagram) ) { ?>
+		<li class="social_list__item">
+			<a class="social_list__icon social_list__icon--instagram" href="//www.instagram.com/<?php echo $instagram; ?>" target="_blank"></a>
+		</li>
+	<?php } ?>
 </ul>
