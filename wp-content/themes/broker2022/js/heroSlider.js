89a471ee5bf653d567b9597a76e2dd88188f99a5
Dennis FOX <dfox@foxartbox.com>
2022-05-31 09:59:59 +0300

    Hero slider JS to independent file.

---
+++ wp-content/themes/broker2022/js/heroSlider.js
@@ -0,0 +1,27 @@
+
+// some special code here
+
+// more info
+// https://github.com/idangerous/plugins/tree/master/Chop%20Slider%203/Chop%20Slider%203%20jQuery
+$(document).ready(function() {
+	$('.js-hero-slider').cs3({
+		autoplay: {
+			delay: '4000',
+			enabled: 'true'
+		},
+		captions: {
+			enabled: 'true'
+		},
+		// effects: 'bricks3d',
+		// effects: 'bulb',
+		effects: 'blinds_h',
+		navigation : {
+			next: '.js-hero-slider-next',
+			prev: '.js-hero-slider-prev'
+		},
+		pagination: {
+			container: '.js-hero-slider-pagination'
+		},
+		preloader: 'false'
+	})
+})
