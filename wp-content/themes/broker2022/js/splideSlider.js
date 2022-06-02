d98d3ffdef84e237ea765fff271bcfdaef0c89c6
Dennis FOX <dfox@foxartbox.com>
2022-06-02 15:21:42 +0300

    Splide slider JS.

---
+++ wp-content/themes/broker2022/js/splideSlider.js
@@ -0,0 +1,7 @@
+
+// https://splidejs.com/guides/getting-started/
+let splideSliders = document.getElementsByClassName('js-splide-slider')
+
+for (let i = 0; i < splideSliders.length; i++) {
+	let splide = new Splide(splideSliders[ i ]).mount({})
+}
