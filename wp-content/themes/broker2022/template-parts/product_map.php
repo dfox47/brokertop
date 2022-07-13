1582f0b8896ab74e70a005e9918097bf8b445691
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-07-13 11:55:34 +0300

    Product map styles.

--- wp-content/themes/broker2022/template-parts/product_map.php
+++ wp-content/themes/broker2022/template-parts/product_map.php
@@ -4,20 +4,23 @@ global $product;
 
 $googleMapsX = (empty($product->get_attribute('pa_google-api-x')) ? '55.7560299' : $product->get_attribute('pa_google-api-x'));
 $googleMapsY = (empty($product->get_attribute('pa_google-api-y')) ? '37.6048052' : $product->get_attribute('pa_google-api-y'));
-$googleAdress = (empty($product->get_attribute('pa_adres')) ? '' : $product->get_attribute('pa_adres'));
-?>
+$googleAdress = (empty($product->get_attribute('pa_adres')) ? '' : $product->get_attribute('pa_adres')); ?>
 
-<div class="product_map" id="product_map"></div>
+<div id="product_map" class="product_map"></div>
 
-<script src="https://api-maps.yandex.ru/2.1/?apikey=eed2b879-5209-4581-8167-71583aa7db36&lang=ru_RU" type="text/javascript"></script>
+<script src="https://api-maps.yandex.ru/2.1/?apikey=eed2b879-5209-4581-8167-71583aa7db36&lang=ru_RU"></script>
 
 <script>
 	ymaps.ready(function () {
-		var map = new ymaps.Map("product_map", {
-			center: [<?=$googleMapsX;?>, <?=$googleMapsY;?>], 
+		var map = new ymaps.Map('product_map', {
+			center: [<?=$googleMapsX;?>, <?=$googleMapsY;?>],
+			'stylers': {
+				"hue": "#ddb472",
+				'lightness': 1
+			},
 			zoom: 13
 		});
-		
+
 		map.geoObjects.add(new ymaps.Placemark([<?=$googleMapsX;?>, <?=$googleMapsY;?>], {
 				hintContent: "<?=$googleAdress;?>"
 			},{
