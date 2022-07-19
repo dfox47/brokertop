4e9b8ebeb276f06c50a69e6933abaaaa853b7583
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-07-19 17:45:04 +0300

    Go to selected slide

--- wp-content/themes/broker2022/js/splideSlider.js
+++ wp-content/themes/broker2022/js/splideSlider.js
@@ -4,4 +4,11 @@ let splideSliders = document.getElementsByClassName('js-splide-slider')
 
 for (let i = 0; i < splideSliders.length; i++) {
 	let splide = new Splide(splideSliders[ i ]).mount({})
+
+	// go to selected slide
+	$('.js-go-to-slide').click(function () {
+		let slideId = $(this).attr('data-slide')
+
+		splide.go(slideId)
+	})
 }
