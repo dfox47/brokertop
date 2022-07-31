b92da900b3504bc4f359200d6c737cf975bdd421
dfox <dfox@foxartbox.com>
2022-07-31 17:35:37 +0300

    Hero slide. Part 2

--- wp-content/themes/broker2022/template-parts/hero.php
+++ wp-content/themes/broker2022/template-parts/hero.php
@@ -1,58 +1,27 @@
 
-<?php
-$i = '/wp-content/themes/broker2022/i';
-$heroImages = $i . '/hero';
-?>
+<?php $i = '/wp-content/themes/broker2022/i/hero'; ?>
 
 <div class="hero_block_slider">
 	<div class="cs3-wrap">
 		<div class="cs3 js-hero-slider">
-			<?php $args = array(
-				'orderby'           => 'rand',
-				'post_type'         => 'product',
-				'posts_per_page'    => -1,
-//				'product_cat'       => 'kupit',
-				'product_cat'       => 'arendovat'
-			);
-
-			$loop = new WP_Query( $args );
-
-			// thumb
-//			while ($loop->have_posts()) :
-//				$loop->the_post();
-//				global $product; ?>
-<!---->
-<!--				<div class="cs3-slide">-->
-<!--					--><?php //// thumb img url
-//					$thumbUrl = '';
-//
-//					if (get_the_post_thumbnail_url()) {
-//						$thumbUrl = str_replace('https://' . $_SERVER['SERVER_NAME'], '', get_the_post_thumbnail_url()); ?>
-<!--						<div class="hero_block_slide">-->
-<!--							<img class="hero_block_slide__img" src="--><?php //echo $thumbUrl; ?><!--" alt="" />-->
-<!--						</div>-->
-<!--					--><?php //} ?>
-<!--				</div>-->
-<!--			--><?php //endwhile; ?>
-
 			<div class="cs3-slide">
-				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $heroImages; ?>/17.jpg" alt="" /></div>
+				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $i; ?>/17.jpg" alt="" /></div>
 			</div>
 
 			<div class="cs3-slide">
-				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $heroImages; ?>/8.jpg" alt="" /></div>
+				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $i; ?>/8.jpg" alt="" /></div>
 			</div>
 
 			<div class="cs3-slide">
-				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $heroImages; ?>/3.jpg" alt="" /></div>
+				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $i; ?>/3.jpg" alt="" /></div>
 			</div>
 
 			<div class="cs3-slide">
-				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $heroImages; ?>/18.jpg" alt="" /></div>
+				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $i; ?>/18.jpg" alt="" /></div>
 			</div>
 
 			<div class="cs3-slide">
-				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $heroImages; ?>/1.jpg" alt="" /></div>
+				<div class="hero_block_slide"><img class="hero_block_slide__img" src="<?php echo $i; ?>/1.jpg" alt="" /></div>
 			</div>
 
 
@@ -67,7 +36,13 @@ $heroImages = $i . '/hero';
 					<div class="hero_block_slide__content">
 						<div class="hero_block_slide__title">Каталог недвижимости</div>
 
-						<a class="hero_block_slide__developer" href="//novostroiki-moscow.com/">Новостройки</a>
+						<div class="hero_block_slide__developer">
+							<a class="hero_block_slide__developer_link" href="//novostroiki-moscow.com/">Новостройки</a>
+						</div>
+
+						<div class="hero_block_slide__thumb_wrap">
+							<span class="hero_block_slide__thumb" style="background-image: url(<?php echo $i; ?>/8.jpg"></span>
+						</div>
 
 						<div class="hero_block_slide__more"><a class="btn btn_more" href="/shop/">Открыть</a></div>
 					</div>
@@ -77,7 +52,13 @@ $heroImages = $i . '/hero';
 					<div class="hero_block_slide__content">
 						<div class="hero_block_slide__title">Новостройки</div>
 
-						<a class="hero_block_slide__developer" href="//dubaiestate.top/">Дубай/Dubai</a>
+						<div class="hero_block_slide__developer">
+							<a class="hero_block_slide__developer_link" href="//dubaiestate.top/">Дубай / Dubai</a>
+						</div>
+
+						<div class="hero_block_slide__thumb_wrap">
+							<span class="hero_block_slide__thumb" style="background-image: url(<?php echo $i; ?>/3.jpg"></span>
+						</div>
 
 						<div class="hero_block_slide__more"><a class="btn btn_more" href="//novostroiki-moscow.com/">Открыть</a></div>
 					</div>
@@ -85,9 +66,15 @@ $heroImages = $i . '/hero';
 
 				<div class="cs3-caption">
 					<div class="hero_block_slide__content">
-						<div class="hero_block_slide__title">Дубай/Dubai</div>
+						<div class="hero_block_slide__title">Дубай / Dubai</div>
 
-						<a class="hero_block_slide__developer" href="/o-kompanii/">О компании</a>
+						<div class="hero_block_slide__developer">
+							<a class="hero_block_slide__developer_link" href="/o-kompanii/">О компании</a>
+						</div>
+
+						<div class="hero_block_slide__thumb_wrap">
+							<span class="hero_block_slide__thumb" style="background-image: url(<?php echo $i; ?>/18.jpg"></span>
+						</div>
 
 						<div class="hero_block_slide__more"><a class="btn btn_more" href="//dubaiestate.top/">Открыть</a></div>
 					</div>
@@ -97,7 +84,13 @@ $heroImages = $i . '/hero';
 					<div class="hero_block_slide__content">
 						<div class="hero_block_slide__title">О компании</div>
 
-						<a class="hero_block_slide__developer" href="/novosti/">Новости</a>
+						<div class="hero_block_slide__developer">
+							<a class="hero_block_slide__developer_link" href="/novosti/">Новости</a>
+						</div>
+
+						<div class="hero_block_slide__thumb_wrap">
+							<span class="hero_block_slide__thumb" style="background-image: url(<?php echo $i; ?>/1.jpg"></span>
+						</div>
 
 						<div class="hero_block_slide__more"><a class="btn btn_more" href="/o-kompanii/">Открыть</a></div>
 					</div>
@@ -107,47 +100,17 @@ $heroImages = $i . '/hero';
 					<div class="hero_block_slide__content">
 						<div class="hero_block_slide__title">Новости</div>
 
-						<a class="hero_block_slide__developer" href="/shop/">Каталог недвижимости</a>
+						<div class="hero_block_slide__developer">
+							<a class="hero_block_slide__developer_link" href="/shop/">Каталог недвижимости</a>
+						</div>
+
+						<div class="hero_block_slide__thumb_wrap">
+							<span class="hero_block_slide__thumb" style="background-image: url(<?php echo $i; ?>/17.jpg"></span>
+						</div>
 
 						<div class="hero_block_slide__more"><a class="btn btn_more" href="/novosti/">Открыть</a></div>
 					</div>
 				</div>
-
-<!--				--><?php //while ($loop->have_posts()) :
-//					$loop->the_post();
-//					global $product; ?>
-<!---->
-<!--					<div class="cs3-caption">-->
-<!--						<div class="hero_block_slide__content">-->
-<!--							<div class="hero_block_slide__title">--><?php //the_title(); ?><!--</div>-->
-<!---->
-<!--							--><?php //// address
-//							if ($product->get_attribute('pa_adres')) { ?>
-<!--								<div class="hero_block_slide__address"><span>--><?php //echo $product->get_attribute('pa_adres'); ?><!--</span></div>-->
-<!--							--><?php //} ?>
-<!---->
-<!--							<div class="hero_block_slide__more">-->
-<!--								<a class="btn btn_more" href="--><?php //echo get_permalink($loop->post->ID) ?><!--">Подробнее</a>-->
-<!--							</div>-->
-<!---->
-<!--							--><?php //// developer
-//							if ($product->get_attribute('zastrojshhik')) { ?>
-<!--								<div class="hero_block_slide__developer">--><?php //echo $product->get_attribute('zastrojshhik'); ?><!--</div>-->
-<!--							--><?php //} ?>
-<!---->
-<!--							--><?php //// 1st gallery image
-//							$gallery_images = $product->get_gallery_image_ids();
-//
-//							if ( is_array( $gallery_images ) && !empty($gallery_images) ) {
-//								$image_link = str_replace('https://' . $_SERVER['SERVER_NAME'], '', wp_get_attachment_url($gallery_images[0])); ?>
-<!---->
-<!--								<div class="hero_block_slide__thumb_wrap">-->
-<!--									<span class="hero_block_slide__thumb" style="background-image: url(--><?php //echo $image_link; ?><!--"></span>-->
-<!--								</div>-->
-<!--							--><?php //} ?>
-<!--						</div>-->
-<!--					</div>-->
-<!--				--><?php //endwhile; ?>
 			</div>
 		</div>
 	</div>
