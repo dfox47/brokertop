74f846dc07368dd6e7f86605eb19e5c341d2959f
dfox <dfox@foxartbox.com>
2022-09-19 09:08:32 +0300

    Menu to separate JS

---
+++ wp-content/themes/broker2022/js/menu.js
@@ -0,0 +1,39 @@
+
+let $menuItem153 = document.querySelector('.menu-item-153 a')
+let $menuItem154 = document.querySelector('.menu-item-154 a')
+
+$menuItem153.addEventListener('click', (e) => {
+	e.preventDefault()
+
+	// remove old tip
+	menuTipRemove()
+	menuTipCreate($menuItem153)
+})
+
+$menuItem154.addEventListener('click', (e) => {
+	e.preventDefault()
+
+	// remove old tip
+	menuTipRemove()
+	menuTipCreate($menuItem154)
+})
+
+let menuTipCreate = (menuId) => {
+	let menuTip = document.createElement('div')
+	menuTip.classList.add('header_menu_tip')
+	menuTip.textContent = 'Раздел на модерации'
+
+	insertAfter(menuTip, menuId)
+
+	setTimeout(() => {
+		menuTip.remove()
+	}, 2000)
+}
+
+let menuTipRemove = () => {
+	let menuOld = document.querySelector('.header_menu_tip')
+
+	if (menuOld) {
+		menuOld.remove()
+	}
+}
