375baf3df36e5c12b965b53870b267ee0040f969
dfox <dfox@foxartbox.com>
2022-10-22 10:15:25 +0300

    Script to lazy load of images

---
+++ wp-content/themes/broker2022/js/imgScroll.js
@@ -0,0 +1,19 @@
+// pure JS version
+let imgScroll = () => {
+	document.querySelectorAll('.js-img-scroll').forEach((e) => {
+		if (window.pageYOffset + window.innerHeight > e.offsetTop) {
+			e.classList.remove('js-img-scroll')
+			e.src = e.dataset.src
+		}
+	})
+}
+
+imgScroll()
+
+window.addEventListener('resize', function() {
+	imgScroll()
+})
+
+window.addEventListener('scroll', function() {
+	imgScroll()
+})
