43fd6566138632ede61ae019538dff2ae6ca0a29
dfox <dfox@foxartbox.com>
2023-03-23 23:40:35 +0200

    Popups for new pages

--- wp-content/themes/broker2022/template-parts/popups.php
+++ wp-content/themes/broker2022/template-parts/popups.php
@@ -25,7 +25,7 @@
 </div>
 
 <?php // Добро пожаловать ?>
-<div class="popup popup--welcome js-popup" data-popup="feedback4">
+<div class="popup popup--welcome js-popup" data-popup="feedback-6847">
 	<div class="popup__bg js-popup-close"></div>
 
 	<div class="popup__content">
@@ -36,3 +36,30 @@
 		</div>
 	</div>
 </div>
+
+<?php // Внимание! Закрытые лоты и переуступки с лучшей ценой! ?>
+<div class="popup popup--welcome js-popup" data-popup="feedback-6953">
+	<div class="popup__bg js-popup-close"></div>
+
+	<div class="popup__content">
+		<div class="popup__close js-popup-close"></div>
+
+		<div class="contacts_form">
+			<?php echo do_shortcode('[contact-form-7 id="6953"]'); ?>
+		</div>
+	</div>
+</div>
+
+<?php // Внимание! Успейте забронировать до старта продаж! ?>
+<div class="popup popup--welcome js-popup" data-popup="feedback-6954">
+	<div class="popup__bg js-popup-close"></div>
+
+	<div class="popup__content">
+		<div class="popup__close js-popup-close"></div>
+
+		<div class="contacts_form">
+			<?php echo do_shortcode('[contact-form-7 id="6954"]'); ?>
+		</div>
+	</div>
+</div>
+
