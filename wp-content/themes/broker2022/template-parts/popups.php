c4bbf461aa7bc626bb882dfcb8ea7778c12c2d89
dfox <dfox@foxartbox.com>
2023-03-23 21:01:43 +0200

    Welcome popup

--- wp-content/themes/broker2022/template-parts/popups.php
+++ wp-content/themes/broker2022/template-parts/popups.php
@@ -1,4 +1,4 @@
-<?php // feedback 2 popup ?>
+<?php // Подать заявку ?>
 <div class="popup popup--feedback js-popup" data-popup="feedback2">
 	<div class="popup__bg js-popup-close"></div>
 
@@ -11,7 +11,7 @@
 	</div>
 </div>
 
-<?php // feedback 3 popup ?>
+<?php // Реализовать объект ?>
 <div class="popup popup--feedback js-popup" data-popup="feedback3">
 	<div class="popup__bg js-popup-close"></div>
 
@@ -20,6 +20,19 @@
 
 		<div class="contacts_form">
 			<?php echo do_shortcode('[contact-form-7 id="1967"]'); ?>
+		</div>
+	</div>
+</div>
+
+<?php // Добро пожаловать ?>
+<div class="popup popup--welcome js-popup" data-popup="feedback4">
+	<div class="popup__bg js-popup-close"></div>
+
+	<div class="popup__content">
+		<div class="popup__close js-popup-close"></div>
+
+		<div class="contacts_form">
+			<?php echo do_shortcode('[contact-form-7 id="6847"]'); ?>
 		</div>
 	</div>
 </div>
