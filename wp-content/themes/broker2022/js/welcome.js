11fcb4f1ac6274cf4fc63c6461fc9ce1f4b5557f
dfox <dfox@foxartbox.com>
2023-04-09 12:53:03 +0300

    Welcome. JS ref. Part 3

--- wp-content/themes/broker2022/js/welcome.js
+++ wp-content/themes/broker2022/js/welcome.js
@@ -4,22 +4,24 @@ const welcomeAnimation = () => {
 
 	if ($welcome == null) return
 
+	const $html = document.querySelector('html')
+
 	// check that all images are loaded
 	Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
 		// animation part 1
-		document.querySelector('html').classList.add('welcome_active')
+		$html.classList.add('welcome_active')
 
 		// animation part 2
 		setTimeout(() => {
-			document.querySelector('html').classList.add('welcome_part_2')
+			$html.classList.add('welcome_part_2')
 
 			document.querySelector('.js-welcome-preload').remove()
 		}, 6000)
 
 		// animation part 3 | hide & remove
 		setTimeout(() => {
-			document.querySelector('html').classList.remove('welcome_active')
-			document.querySelector('html').classList.remove('welcome_part_2')
+			$html.classList.remove('welcome_active')
+			$html.classList.remove('welcome_part_2')
 
 			document.querySelector('.js-welcome').remove()
 		}, 9000)
