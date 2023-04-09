825446329ded69cd935f96459bde2175d8281d88
dfox <dfox@foxartbox.com>
2023-04-09 23:23:36 +0300

    Slider. New buildings. New bg

--- wp-content/themes/broker2022/template-parts/new_buildings.php
+++ wp-content/themes/broker2022/template-parts/new_buildings.php
@@ -6,7 +6,7 @@ $dumm   = '/wp-content/themes/broker2022/i/dumm.png';
 <div class="hero_block_slider">
 	<div class="owl-carousel js-owl-carousel">
 		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/17.jpg" alt="">
+			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/20.jpg" alt="">
 
 			<div class="hero_block_slide__content">
 				<div class="hero_block_slide__title">Новостройки</div>
@@ -18,74 +18,10 @@ $dumm   = '/wp-content/themes/broker2022/i/dumm.png';
 		</div>
 
 		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/8.jpg" alt="">
+			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/21.jpg" alt="">
 
 			<div class="hero_block_slide__content">
-				<div class="hero_block_slide__title">Новостройки</div>
-
-				<div class="hero_block_slide__developer">
-					<a class="hero_block_slide__developer_link js-menu-item-disabled" href="//dubaiestate.top/">Дубай / Dubai</a>
-				</div>
-
-				<div class="hero_block_slide__thumb_wrap">
-					<span class="hero_block_slide__thumb js-img-bg" data-src="<?= $i; ?>/3.jpg"></span>
-				</div>
-
-				<div class="hero_block_slide__more"><a class="btn btn_more js-menu-item-disabled" href="//novostroiki-moscow.com/">Открыть</a></div>
-			</div>
-		</div>
-
-		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/3.jpg" alt="">
-
-			<div class="hero_block_slide__content">
-				<div class="hero_block_slide__title">Дубай / Dubai</div>
-
-				<div class="hero_block_slide__developer">
-					<a class="hero_block_slide__developer_link" href="/o-kompanii/">О компании</a>
-				</div>
-
-				<div class="hero_block_slide__thumb_wrap">
-					<span class="hero_block_slide__thumb js-img-bg" data-src="<?= $i; ?>/18.jpg"></span>
-				</div>
-
-				<div class="hero_block_slide__more"><a class="btn btn_more js-menu-item-disabled" href="//dubaiestate.top/">Открыть</a></div>
-			</div>
-		</div>
-
-		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/18.jpg" alt="">
-
-			<div class="hero_block_slide__content">
-				<div class="hero_block_slide__title">О компании</div>
-
-				<div class="hero_block_slide__developer">
-					<a class="hero_block_slide__developer_link" href="/novosti/">Новости</a>
-				</div>
-
-				<div class="hero_block_slide__thumb_wrap">
-					<span class="hero_block_slide__thumb js-img-bg" data-src="<?= $i; ?>/1.jpg"></span>
-				</div>
-
-				<div class="hero_block_slide__more"><a class="btn btn_more" href="/o-kompanii/">Открыть</a></div>
-			</div>
-		</div>
-
-		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/1.jpg" alt="">
-
-			<div class="hero_block_slide__content">
-				<div class="hero_block_slide__title">Новости</div>
-
-				<div class="hero_block_slide__developer">
-					<a class="hero_block_slide__developer_link" href="/shop/">Каталог недвижимости</a>
-				</div>
-
-				<div class="hero_block_slide__thumb_wrap">
-					<span class="hero_block_slide__thumb js-img-bg" data-src="<?= $i; ?>/17.jpg"></span>
-				</div>
-
-				<div class="hero_block_slide__more"><a class="btn btn_more" href="/novosti/">Открыть</a></div>
+				<?= do_shortcode('[contact-form-7 id="6954"]'); ?>
 			</div>
 		</div>
 	</div>
