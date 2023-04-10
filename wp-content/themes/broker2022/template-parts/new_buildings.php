00016ac440bc4d02a74c30dcd160bd8e02ac2ba3
dfox <dfox@foxartbox.com>
2023-04-10 21:22:16 +0300

    Look form. Part 5

--- wp-content/themes/broker2022/template-parts/new_buildings.php
+++ wp-content/themes/broker2022/template-parts/new_buildings.php
@@ -5,14 +5,6 @@ $dumm   = '/wp-content/themes/broker2022/i/dumm.png';
 
 <div class="hero_block_slider">
 	<div class="owl-carousel js-owl-carousel">
-		<div class="hero_block_slide hero_block_slide--light">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/21.jpg" alt="">
-
-			<div class="hero_block_slide__content">
-				<?= do_shortcode('[contact-form-7 id="7237"]'); ?>
-			</div>
-		</div>
-
 		<div class="hero_block_slide">
 			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/20.jpg" alt="">
 
@@ -22,6 +14,14 @@ $dumm   = '/wp-content/themes/broker2022/i/dumm.png';
 				<div class="hero_block_slide__desc">Квартиры и апартаменты в лучших жилых комплексах Москвы и Подмосковья</div>
 
 				<div class="hero_block_slide__more"><a class="btn btn_more" href="/shop/">Получить планировки и цены</a></div>
+			</div>
+		</div>
+
+		<div class="hero_block_slide hero_block_slide--light">
+			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/21.jpg" alt="">
+
+			<div class="hero_block_slide__content">
+				<?= do_shortcode('[contact-form-7 id="7237"]'); ?>
 			</div>
 		</div>
 	</div>
