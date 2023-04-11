aad790dd24a7c58ce116269379ef025acea5ba23
dfox <dfox@foxartbox.com>
2023-04-11 17:04:52 +0300

    New building. List. Part 1

--- wp-content/themes/broker2022/page-new_buildings.php
+++ wp-content/themes/broker2022/page-new_buildings.php
@@ -19,21 +19,29 @@
 			));
 
 			foreach ($categories as $category) {
-				$desc       = $category->description;
-				$id         = $category->term_id;
-				$image      = wp_get_attachment_url(get_term_meta($id, 'thumbnail_id', true));
-				$link       = get_category_link($id);
-				$name       = $category->name;
+				$desc           = $category->description;
+				$desc_no_img    = preg_replace('/(<)([img])(\w+)([^>]*>)/', '', $desc);
+				$id             = $category->term_id;
+				$image          = wp_get_attachment_url(get_term_meta($id, 'thumbnail_id', true));
+				$link           = get_category_link($id);
+				$name           = $category->name;
 
-				if ($image) {
-					echo '<img src="' . $image . '" alt="">';
-				} ?>
+				preg_match('@src="([^"]+)"@', $desc, $match);
 
-				<li class="new_building_list__item"></li>
+				$logo_link = array_pop($match); ?>
 
-				<?php echo '<p><a href="' . $link . '" title="' . $name . '" ' . '>' . $name . '</a></p>';
-				echo '<p>' . $category->description . '</p><br><br><br><br><br>';
-			} ?>
+				<li class="new_building_list__item">
+					<span class="new_building_list__img">
+						<?php if ($image) { ?>
+							<img src="<?=  $image; ?>" alt="">
+						<?php } ?>
+					</span>
+
+					<span class="new_building_list__logo"><img src="<?= $logo_link; ?>" alt=""></span>
+
+					<span class="new_building_list__desc"><?= $desc_no_img; ?></span>
+				</li>
+			<?php } ?>
 		</ul>
 	</div>
 
