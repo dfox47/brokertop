c7524a4ff55e62a8c61f59b63bf2c3073dc5ed54
dfox <dfox@foxartbox.com>
2023-04-11 15:19:33 +0300

    New building. Quote. Part 1

---
+++ wp-content/themes/broker2022/page-new_buildings.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Template Name: Новостройки
+* Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+<h1>Жилые комплексы</h1>
+
+<?php // content
+the_content(); ?>
+
+<?php get_footer(); ?>
