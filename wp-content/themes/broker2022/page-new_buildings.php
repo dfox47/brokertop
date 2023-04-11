22677032e39d8e280425733c48a2ca53d55d6705
dfox <dfox@foxartbox.com>
2023-04-11 17:56:30 +0300

    New building. List. Part 8

--- wp-content/themes/broker2022/page-new_buildings.php
+++ wp-content/themes/broker2022/page-new_buildings.php
@@ -13,7 +13,7 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 	<h1>Жилые комплексы</h1>
 
 	<div class="wrap">
-		<ul class="new_building_list owl-carousel js-owl-buildings">
+		<div class="new_building_list owl-carousel js-owl-buildings">
 			<?php // subcategories of new buildings
 			$categories = get_terms(array(
 				'hide_empty'    => false,
@@ -21,11 +21,12 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 				'taxonomy'      => 'product_cat'
 			));
 
-			foreach ($categories as $category) {
+			foreach ($categories as $key => $category) {
 				$desc           = $category->description;
 				$desc_no_img    = preg_replace('/(<)([img])(\w+)([^>]*>)/', '', $desc);
 				$id             = $category->term_id;
 				$image          = wp_get_attachment_url(get_term_meta($id, 'thumbnail_id', true));
+				$index          = 0;
 				$link           = get_category_link($id);
 				$name           = $category->name;
 
@@ -33,21 +34,26 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 
 				$logo_link = array_pop($match); ?>
 
-				<li class="new_building_list__item">
-					<a class="new_building_list__link" href="<?= $link; ?>">
+				<div class="new_building_list__group">
+					<div class="new_building_list__item">
+						<a class="new_building_list__link" href="<?= $link; ?>">
 						<span class="new_building_list__img">
 							<?php if ($image) { ?>
 								<img class="js-img-scroll" src="<?= $dumm; ?>" data-src="<?=  $image; ?>" alt="">
 							<?php } ?>
 						</span>
 
-						<span class="new_building_list__logo"><img class="js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $logo_link; ?>" alt=""></span>
+							<span class="new_building_list__logo"><img class="js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $logo_link; ?>" alt=""></span>
 
-						<span class="new_building_list__desc"><?= $desc_no_img; ?></span>
-					</a>
-				</li>
-			<?php } ?>
-		</ul>
+							<span class="new_building_list__desc"><?= $desc_no_img; ?></span>
+						</a>
+					</div>
+				</div>
+
+				<?php // increase index
+				$index++;
+			} ?>
+		</div>
 	</div>
 
 <?php // content
