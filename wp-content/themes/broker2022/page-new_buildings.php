ea2b92cc3b81cc6f1ee9beb6db95ca5bc6aaedc8
dfox <dfox@foxartbox.com>
2023-04-11 16:41:48 +0300

    New building. Quote. Part 3

--- wp-content/themes/broker2022/page-new_buildings.php
+++ wp-content/themes/broker2022/page-new_buildings.php
@@ -7,7 +7,35 @@
 
 <?php get_header(); ?>
 
-<h1>Жилые комплексы</h1>
+	<h1>Жилые комплексы</h1>
+
+	<div class="wrap">
+		<ul class="new_building_list">
+			<?php // subcategories of new buildings
+			$categories = get_terms(array(
+				'hide_empty'    => false,
+				'parent'        => 2291,
+				'taxonomy'      => 'product_cat'
+			));
+
+			foreach ($categories as $category) {
+				$desc       = $category->description;
+				$id         = $category->term_id;
+				$image      = wp_get_attachment_url(get_term_meta($id, 'thumbnail_id', true));
+				$link       = get_category_link($id);
+				$name       = $category->name;
+
+				if ($image) {
+					echo '<img src="' . $image . '" alt="">';
+				} ?>
+
+				<li class="new_building_list__item"></li>
+
+				<?php echo '<p><a href="' . $link . '" title="' . $name . '" ' . '>' . $name . '</a></p>';
+				echo '<p>' . $category->description . '</p><br><br><br><br><br>';
+			} ?>
+		</ul>
+	</div>
 
 <?php // content
 the_content(); ?>
