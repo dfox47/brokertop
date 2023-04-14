54dfc63558dbcb0f0f6cab2fd30b3f31d9fa8dfe
dfox <dfox@foxartbox.com>
2023-04-14 21:09:43 +0300

    Foreign. Part 3

--- wp-content/themes/broker2022/page-foreign.php
+++ wp-content/themes/broker2022/page-foreign.php
@@ -10,44 +10,45 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 
 <?php get_header(); ?>
 
-	<div class="wrap_bg">
-		<h1>Жилые комплексы</h1>
+<div class="wrap_bg">
+	<h1>Жилые комплексы</h1>
 
-		<div class="wrap">
-			<div class="new_building_list owl-carousel js-owl-buildings">
-				<div class="new_building_list__group">
-					<?php
-					$args = array(
-						'posts_per_page'    => 40,
-						'product_cat'       => 'zarubezhnaya-nedvizhimost'
-					);
+	<div class="wrap">
+		<div class="new_building_list owl-carousel js-owl-buildings">
+			<?php $args = array(
+				'posts_per_page'    => 40,
+				'product_cat'       => 'zarubezhnaya-nedvizhimost'
+			);
 
-					$loop = new WP_Query($args);
-					$index = 0;
+			$loop = new WP_Query($args);
+			$index = 0;
 
-					while ($loop->have_posts()) : $loop->the_post();
-					global $product;
+			while ($loop->have_posts()) : $loop->the_post();
+				global $product;
 
-					$id             = $loop->post->ID;
-					$desc           = $product->get_short_description();
-					$descNoImg      = preg_replace('/(<)([img])(\w+)([^>]*>)/', '', $desc);
-					$image          = get_the_post_thumbnail($id, 'large');
-					$link           = get_permalink($id);
-					$attributes     = $product->get_attributes();
-					$realtorPhone   = preg_replace('/\D/', '', $product->get_attribute('pa_telefon-rieltora'));
-					$pdfLink        = $product->get_attribute('pa_ssylka-na-prezentacziyu') ?: '/ajax_presentation.php?id=' . $id;
+				$id             = $loop->post->ID;
+				$desc           = $product->get_short_description();
+				$descNoImg      = preg_replace('/(<)([img])(\w+)([^>]*>)/', '', $desc);
+				$image          = get_the_post_thumbnail($id, 'large');
+				$link           = get_permalink($id);
+				$attributes     = $product->get_attributes();
+				$realtorPhone   = preg_replace('/\D/', '', $product->get_attribute('pa_telefon-rieltora'));
+				$pdfLink        = $product->get_attribute('pa_ssylka-na-prezentacziyu') ?: '/ajax_presentation.php?id=' . $id;
 
-					preg_match('@src="([^"]+)"@', $desc, $match);
-					$logoLink       = array_pop($match);
-					?>
+				preg_match('@src="([^"]+)"@', $desc, $match);
+				$logoLink       = array_pop($match); ?>
 
-					<div class="new_building_list__item">
-						<div class="new_building_list__link">
-							<span class="new_building_list__img"><?php if ($image) echo $image; ?></span>
+				<?php if ($index % 2 !== 1) { ?>
+					<div class="new_building_list__group">
+				<?php } ?>
 
-							<span class="new_building_list__logo"><img class="js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $logoLink; ?>" alt=""></span>
+				<div class="new_building_list__item" data-index="<?= $index; ?>">
+					<div class="new_building_list__link">
+						<span class="new_building_list__img"><?php if ($image) echo $image; ?></span>
 
-							<span class="new_building_list__desc">
+						<span class="new_building_list__logo"><img class="js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $logoLink; ?>" alt=""></span>
+
+						<span class="new_building_list__desc">
 								<?= $descNoImg; ?>
 
 								<span class="new_building_links">
@@ -62,24 +63,27 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 									<a class="new_building_links__item social_list__icon social_list__icon--telegram" href="//t.me/top_broker_estate" target="_blank" rel="noopener" title="telegram"></a>
 								</span>
 							</span>
-						</div>
 					</div>
-
-					<?php if ($index % 2 == 1) { ?>
 				</div>
-				<div class="new_building_list__group">
-					<?php } ?>
 
-					<?php $index++; ?>
-					<?php endwhile; ?>
+				<?php if ($index % 2 == 1) { ?>
+					</div>
+				<?php } ?>
 
-					<?php wp_reset_query(); ?>
+				<?php $index++; ?>
+			<?php endwhile; ?>
+
+			<?php if ($index % 2 !== 1) { ?>
 				</div>
-			</div>
+			<?php } ?>
+
+			<?php wp_reset_query(); ?>
 		</div>
+	</div>
+</div>
 
-		<?php // content
-		the_content(); ?>
+<?php // content
+the_content(); ?>
 	</div>
 
 <?php get_footer(); ?>
