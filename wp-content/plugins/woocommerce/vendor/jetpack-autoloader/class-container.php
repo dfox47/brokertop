cbb479d55ad209dda480bfd5143ae048996355ac
dfox <dfox@foxartbox.com>
2023-04-16 15:57:39 +0300

    Plugins updates. Part 2

--- wp-content/plugins/woocommerce/vendor/jetpack-autoloader/class-container.php
+++ wp-content/plugins/woocommerce/vendor/jetpack-autoloader/class-container.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp225400540d1b8fa2507bcebf7ea0d4ff;
+namespace Automattic\Jetpack\Autoloader\jpa10d711ec5dd1299c49f4fb90df5a407;
 
  // phpcs:ignore
 
