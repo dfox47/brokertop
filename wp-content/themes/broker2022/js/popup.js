8292073fe70ec51d684a8c59b387584906629bd0
dfox <dfox@foxartbox.com>
2023-04-18 22:30:40 +0300

    Popup. Js ref

--- wp-content/themes/broker2022/js/popup.js
+++ wp-content/themes/broker2022/js/popup.js
@@ -1,31 +1,55 @@
 // popup.js [START]
-const $popups       = document.querySelectorAll('.js-popup')
-const $popupShow    = document.querySelectorAll('.js-popup-show')
-const $popupClose   = document.querySelectorAll('.js-popup-close')
+const $popupClose       = document.querySelectorAll('.js-popup-close')
+const $popupContent     = document.querySelector('.js-popup-content')
+const $popupShow        = document.querySelectorAll('.js-popup-show')
+const $popups           = document.querySelectorAll('.js-popup')
 
+// show popup
 $popupShow.forEach((button) => {
 	const popupId = button.dataset.popup
 
+	if (popupId === '') return
+
 	button.addEventListener('click', () => {
 		$popups.forEach((e) => {
-			let popupSelected = e.dataset.popup
-
-			if (popupSelected === popupId) {
+			// show selected popup
+			if (e.dataset.popup === popupId) {
 				e.classList.add('active')
+
+				// if popup with content to put
+				if (popupId === 'with_content') {
+					const $popupContentToPut = button.querySelector('.js-popup-content-put')
+
+					if ($popupContentToPut == null) return
+
+					$popupContent.innerHTML = $popupContentToPut.innerHTML
+				}
 
 				return
 			}
 
+			// hide all other popups
 			e.classList.remove('active')
 		})
+
+		// add class from html tag
+		document.getElementsByTagName('html')[0].classList.add('popup_active')
 	})
 })
 
+// close popup
 $popupClose.forEach((button) => {
 	button.addEventListener('click', () => {
 		$popups.forEach((e) => {
+			// hide all popups
 			e.classList.remove('active')
+
+			// clear popup content
+			$popupContent.innerHTML = ''
 		})
+
+		// remove class from html tag
+		document.getElementsByTagName('html')[0].classList.remove('popup_active')
 	})
 })
 // popup.js [END]
