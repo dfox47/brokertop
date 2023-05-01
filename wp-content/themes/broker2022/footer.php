7ec001df7f377b80fca449c1a23e20ab5efe80b9
dfox <dfox@foxartbox.com>
2023-05-01 16:23:49 +0300

    JS. Async scripts. Part 2

--- wp-content/themes/broker2022/footer.php
+++ wp-content/themes/broker2022/footer.php
@@ -44,7 +44,7 @@ $whatsapp   = esc_attr(get_option('broker_whatsapp'));
 
 				<a class="footer_agree" href="">Согласие на обработку персональных данных</a>
 
-				<iframe class="yandex_badge" src="//yandex.ru/sprav/widget/rating-badge/4782454911"></iframe>
+				<div class="yandex_badge_wrap js-yandex-badge"></div>
 			</div>
 		</div>
 	</div>
@@ -61,17 +61,6 @@ include "template-parts/popups.php"; ?>
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/all.min.js?v<?php echo(date("Ymd")); ?>"></script>
 
 <?php wp_footer(); ?>
-
-<?php // mango callback | https://lk.mango-office.ru/ ?>
-<script>
-	(function(w, d, u, i, o, s, p) {
-		if (d.getElementById(i)) { return; } w['MangoObject'] = o;
-		w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
-		s = d.createElement('script'); s.async = 1; s.id = i; s.src = u; s.charset = 'utf-8';
-		p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
-	}(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
-	mgo({multichannel: {id: 12601}});
-</script>
 
 </body>
 </html>
