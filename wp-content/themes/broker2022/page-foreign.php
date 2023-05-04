313dc6c2e3d0a5a9b88919d92de5173eece18453
dfox <dfox@foxartbox.com>
2023-05-04 20:59:16 +0300

    foreign page ref

--- wp-content/themes/broker2022/page-foreign.php
+++ wp-content/themes/broker2022/page-foreign.php
@@ -29,10 +29,10 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 				$id             = $loop->post->ID;
 				$desc           = $product->get_short_description();
 				$descNoImg      = preg_replace('/(<)([img])(\w+)([^>]*>)/', '', $desc);
+				$galleryImages  = $product->get_gallery_image_ids();
 				$image          = get_the_post_thumbnail($id, 'large');
 				$link           = get_permalink($id);
-				$attributes     = $product->get_attributes();
-				$realtorPhone   = preg_replace('/\D/', '', $product->get_attribute('pa_telefon-rieltora'));
+				$realtorPhone   = $product->get_attribute('pa_telefon-rieltora') ? preg_replace('/\D/', '', $product->get_attribute('pa_telefon-rieltora')) : '79778021616';
 				$pdfLink        = $product->get_attribute('pa_ssylka-na-prezentacziyu') ?: '/ajax_presentation.php?id=' . $id;
 
 				preg_match('@src="([^"]+)"@', $desc, $match);
@@ -42,7 +42,7 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 					<div class="new_building_list__group">
 				<?php } ?>
 
-				<div class="new_building_list__item" data-index="<?= $index; ?>">
+				<div class="new_building_list__item js-buildings-item js-popup-show" data-popup="with_content">
 					<div class="new_building_list__link">
 						<span class="new_building_list__img"><?php if ($image) echo $image; ?></span>
 
@@ -64,6 +64,34 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 								</span>
 							</span>
 					</div>
+
+					<div class="hidden js-popup-content-put">
+						<div class="product_slider">
+							<div class="owl-carousel js-popup-owl-carousel">
+								<?php // gallery images
+								foreach ($galleryImages as $galleryImage) {
+									// thumbnail | medium | large | full
+									$imageLink = str_replace('https://' . $_SERVER['SERVER_NAME'], '', wp_get_attachment_url($galleryImage)); ?>
+
+									<img class="product_slider__img js-img-lazy" src="<?= $dumm; ?>" data-src="<?= $imageLink; ?>" alt="" />
+								<?php } ?>
+							</div>
+						</div>
+
+						<span class="new_building_popup_links">
+								<?php if ($pdfLink) { ?>
+									<a class="new_building_popup_links__item new_building_popup_link__presentation" href="<?= $pdfLink; ?>" target="_blank">Скачать презентацию</a>
+								<?php } ?>
+
+								<div class="new_building_popup_links__social">
+									<?php if ($realtorPhone) { ?>
+										<a class="new_building_popup_links__item social_list__icon social_list__icon--whatsapp" href="//wa.me/<?= $realtorPhone; ?>" target="_blank" rel="noopener" title="whatsapp"></a>
+									<?php }  ?>
+
+									<a class="new_building_popup_links__item social_list__icon social_list__icon--telegram" href="//t.me/top_broker_estate" target="_blank" rel="noopener" title="telegram"></a>
+								</div>
+							</span>
+					</div>
 				</div>
 
 				<?php if ($index % 2 == 1) { ?>
@@ -74,12 +102,14 @@ $dumm = '/wp-content/themes/broker2022/i/dumm.png'; ?>
 			<?php endwhile; ?>
 
 			<?php if ($index % 2 !== 1) { ?>
-				</div>
-			<?php } ?>
+		</div>
+		<?php } ?>
 
-			<?php wp_reset_query(); ?>
+		<?php wp_reset_query(); ?>
 	</div>
 </div>
+	</div>
+	</div>
 
 <?php // content
 the_content(); ?>
