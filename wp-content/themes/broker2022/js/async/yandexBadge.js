0295fb4b296b5d354e285972cf2a5025a7e75ebb
dfox <dfox@foxartbox.com>
2023-05-05 00:32:31 +0300

    JS yandex fix

--- wp-content/themes/broker2022/js/async/yandexBadge.js
+++ wp-content/themes/broker2022/js/async/yandexBadge.js
@@ -2,6 +2,7 @@ const yandexBadge = () => {
 	document.querySelectorAll('.js-yandex-badge').forEach((e) => {
 		if (window.pageYOffset + window.innerHeight > e.offsetTop) {
 			e.innerHTML = '<iframe class="yandex_badge" src="//yandex.ru/sprav/widget/rating-badge/4782454911"></iframe>'
+			e.classList.remove('js-yandex-badge')
 		}
 	})
 }
