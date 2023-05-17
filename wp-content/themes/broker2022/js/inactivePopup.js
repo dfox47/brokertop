5dd819242a96a362f72a5a243bfd16368374df34
dfox <dfox@foxartbox.com>
2023-05-17 18:39:44 +0300

    Inactive popup. Show only once

--- wp-content/themes/broker2022/js/inactivePopup.js
+++ wp-content/themes/broker2022/js/inactivePopup.js
@@ -1,19 +1,27 @@
-let idleTime = 0
+const inactivePopup = () => {
+	let idleTime = 0
 
-document.addEventListener('mousemove', resetIdleTime, false)
-document.addEventListener('keypress', resetIdleTime, false)
+	if (localStorage.getItem('inactivePopupSeen') === '1') return
 
-function resetIdleTime () {
-	idleTime = 0
-}
+	document.addEventListener('mousemove', resetIdleTime, false)
+	document.addEventListener('keypress', resetIdleTime, false)
 
-function checkIfIdle () {
-	idleTime += 1000
-
-	if (idleTime >= 10000) {
-		$('.js-popup[data-popup="feedback-7237"]').addClass('active')
-		clearInterval(idleInterval)
+	function resetIdleTime () {
+		idleTime = 0
 	}
+
+	function checkIfIdle () {
+		idleTime += 1000
+
+		if (idleTime >= 10000) {
+			$('.js-popup[data-popup="feedback-7237"]').addClass('active')
+			clearInterval(idleInterval)
+
+			localStorage.setItem('inactivePopupSeen', '1')
+		}
+	}
+
+	let idleInterval = setInterval(checkIfIdle, 1000)
 }
 
-let idleInterval = setInterval(checkIfIdle, 1000)
+inactivePopup()
