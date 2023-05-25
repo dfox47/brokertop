6d8bab686ece444bd63e33657268c4eee913a763
dfox <dfox@foxartbox.com>
2023-05-25 21:25:16 +0300

    JS. Inactive popup fix

--- wp-content/themes/broker2022/js/inactivePopup.js
+++ wp-content/themes/broker2022/js/inactivePopup.js
@@ -2,9 +2,6 @@ const inactivePopup = () => {
 	const date          = new Date()
 	const currentTime   = date.getTime()
 
-	console.log('currentTime | ', currentTime)
-	console.log('xxxxx | ', parseInt(localStorage.getItem('inactivePopupSeen')))
-
 	if (parseInt(localStorage.getItem('inactivePopupSeen')) > currentTime) return
 
 	let idleTime = 0
@@ -19,14 +16,11 @@ const inactivePopup = () => {
 	function checkIfIdle () {
 		idleTime += 1000
 
-		console.log('idleTime | ', idleTime)
-
 		if (idleTime >= 10000) {
 			$('.js-popup[data-popup="feedback-7237"]').addClass('active')
 			clearInterval(idleInterval)
 
-			// localStorage.setItem('inactivePopupSeen', (currentTime + 2 * 360000).toString())
-			localStorage.setItem('inactivePopupSeen', (currentTime + 6000).toString())
+			localStorage.setItem('inactivePopupSeen', (currentTime + 2 * 360000).toString())
 		}
 	}
 
