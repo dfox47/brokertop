b24b35b72269b633df29a6f7a30a19eb385ae066
dfox <dfox@foxartbox.com>
2023-05-25 21:05:44 +0300

    Gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,38 @@
-
-let fs                  = require('fs');
-let concat              = require('gulp-concat')
-let config              = JSON.parse(fs.readFileSync('../config.json'))
-let cssMinify           = require('gulp-csso')
-let ftp                 = require('vinyl-ftp')
-let gulp                = require('gulp')
-let gutil               = require('gulp-util')
-let rename              = require('gulp-rename')
-let sass                = require('gulp-sass')(require('sass'))
-let uglify              = require('gulp-uglify')
+const fs            = require('fs')
+const concat        = require('gulp-concat')
+const config        = JSON.parse(fs.readFileSync('../config.json'))
+const cssMinify     = require('gulp-csso')
+const ftp           = require('vinyl-ftp')
+const gulp          = require('gulp')
+const gutil         = require('gulp-util')
+const rename        = require('gulp-rename')
+const sass          = require('gulp-sass')(require('sass'))
+const uglify        = require('gulp-uglify')
 
 // FTP config
-let host                = config.host
-let password            = config.password
-let port                = config.port
-let user                = config.user
+const host          = config.host
+const password      = config.password
+const port          = config.port
+const user          = config.user
 
-let remoteFolder                = '/www/brokertop.ru/wp-content/themes/broker2022/'
-let remoteFolderCss             = remoteFolder + 'css/'
-let remoteFolderJs              = remoteFolder + 'js/'
-let remoteFolderTemplateParts   = remoteFolder + 'template-parts/'
+const remoteMain            = '/www/brokertop.ru/'
+const remoteTheme           = remoteMain + 'wp-content/themes/broker2022/'
+const remoteContact7        = remoteMain + 'wp-content/plugins/contact-form-7/includes/css/'
+const remoteCss             = remoteTheme + 'css/'
+const remoteJs              = remoteTheme + 'js/'
+const remoteTemplateParts   = remoteTheme + 'template-parts/'
+const remoteWCAssets        = remoteMain + 'wp-content/plugins/woocommerce/assets/css/'
+const remoteWCBuild         = remoteMain + 'wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/'
+const remoteWCLibrary       = remoteMain + 'wp-includes/css/dist/block-library/'
 
-let localFolder                 = 'wp-content/themes/broker2022/'
-let localFolderCss              = localFolder + 'css/'
-let localFolderJs               = localFolder + 'js/'
-let localFolderTemplateParts    = localFolder + 'template-parts/'
+const localTheme            = 'wp-content/themes/broker2022/'
+const localContact7         = 'wp-content/plugins/contact-form-7/includes/css/'
+const localCss              = localTheme + 'css/'
+const localJs               = localTheme + 'js/'
+const localTemplateParts    = localTheme + 'template-parts/'
+const localWCAssets         = 'wp-content/plugins/woocommerce/assets/css/'
+const localWCBuild          = 'wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/'
+const localWCLibrary        = 'wp-includes/css/dist/block-library/'
 
 
 
@@ -39,63 +47,114 @@ function getFtpConnection() {
 	})
 }
 
-let conn = getFtpConnection()
+const conn = getFtpConnection()
 
 
 
 gulp.task('css', function () {
-	return gulp.src(localFolderCss + 'styles.scss')
+	return gulp.src(localCss + 'styles.scss')
 		.pipe(sass())
 		.pipe(cssMinify())
 		.pipe(rename({
 			base: 'style'
 			// suffix: ".min"
 		}))
-		.pipe(conn.dest(remoteFolder))
+		.pipe(conn.dest(remoteTheme))
+})
+
+gulp.task('copy_amo_crm', function () {
+	return gulp.src('send_amo.php')
+		.pipe(conn.dest(remoteMain))
 })
 
 gulp.task('copy_css', function () {
-	return gulp.src(localFolderCss + '**/*')
-		.pipe(conn.dest(remoteFolderCss))
+	return gulp.src(localCss + '**/*')
+		.pipe(conn.dest(remoteCss))
+})
+
+gulp.task('copy_css_wc_blocks', function () {
+	return gulp.src(localWCBuild + 'wc-blocks-style.css')
+		.pipe(cssMinify())
+		.pipe(conn.dest(remoteWCBuild))
+})
+
+gulp.task('copy_css_wc_default', function () {
+	return gulp.src(localWCAssets + 'woocommerce.css')
+		.pipe(cssMinify())
+		.pipe(conn.dest(remoteWCAssets))
+})
+
+gulp.task('copy_css_wc_layout', function () {
+	return gulp.src(localWCAssets + 'woocommerce-layout.css')
+		.pipe(cssMinify())
+		.pipe(conn.dest(remoteWCAssets))
+})
+
+gulp.task('copy_css_wc_library', function () {
+	return gulp.src(localWCLibrary + 'style.min.css')
+		.pipe(cssMinify())
+		.pipe(conn.dest(remoteWCLibrary))
+})
+
+gulp.task('copy_css_wc_vendors', function () {
+	return gulp.src(localWCBuild + 'wc-blocks-vendors-style.css')
+		.pipe(cssMinify())
+		.pipe(conn.dest(remoteWCBuild))
+})
+
+gulp.task('copy_css_contact_form_7', function () {
+	return gulp.src(localContact7 + 'styles.css')
+		// .pipe(cssMinify())
+		.pipe(conn.dest(remoteContact7))
 })
 
 gulp.task('copy_html', function () {
-	return gulp.src(localFolder + '*.php')
-		.pipe(conn.dest(remoteFolder))
+	return gulp.src(localTheme + '*.php')
+		.pipe(conn.dest(remoteTheme))
+})
+
+gulp.task('copy_ajax_pdf', function () {
+	return gulp.src('ajax_presentation.php')
+		.pipe(conn.dest(remoteMain))
 })
 
 gulp.task('copy_template_parts', function () {
-	return gulp.src(localFolderTemplateParts + '**/*')
-		.pipe(conn.dest(remoteFolderTemplateParts))
+	return gulp.src(localTemplateParts + '**/*')
+		.pipe(conn.dest(remoteTemplateParts))
 })
 
 gulp.task('copy_js', function () {
-	return gulp.src(localFolderJs + '**/*.js')
-		.pipe(conn.dest(remoteFolderJs))
+	return gulp.src(localJs + '**/*')
+		.pipe(conn.dest(remoteJs))
 })
 
 gulp.task('js', function () {
 	return gulp.src([
-			// localFolderJs + 'jquery.3.2.1.js',
-			localFolderJs + 'owl.carousel.js',
-			localFolderJs + 'jspdf.min.js',
-			localFolderJs + '**/*.js'
+			localJs + 'jquery-3.6.0.min.js',
+			localJs + 'owl.carousel.min.js',
+			localJs + '*.js'
 		])
 		.pipe(concat('all.js'))
-		// .pipe(uglify())
+		.pipe(uglify())
 		.pipe(rename({
 			suffix: ".min"
 		}))
-		.pipe(conn.dest(remoteFolder))
+		.pipe(conn.dest(remoteTheme))
 })
 
 gulp.task('watch', function() {
-	gulp.watch(localFolderCss + '**/*',             gulp.series('css', 'copy_css'))
-	gulp.watch(localFolder + '*.php',               gulp.series('copy_html'))
-	gulp.watch(localFolderJs + '**/*.js',           gulp.series('js', 'copy_js'))
-	gulp.watch(localFolderTemplateParts + '**/*',   gulp.series('copy_template_parts'))
+	gulp.watch('ajax_presentation.php',                         gulp.series('copy_ajax_pdf'))
+	gulp.watch('send_amo.php',                                  gulp.series('copy_amo_crm'))
+	gulp.watch(localContact7 + 'styles.css',                    gulp.series('copy_css_contact_form_7'))
+	gulp.watch(localCss + '**/*',                               gulp.series('css', 'copy_css'))
+	gulp.watch(localJs + '**/*.js',                             gulp.series('js', 'copy_js'))
+	gulp.watch(localTemplateParts + '**/*',                     gulp.series('copy_template_parts'))
+	gulp.watch(localTheme + '*.php',                            gulp.series('copy_html'))
+	gulp.watch(localWCAssets + 'woocommerce-layout.css',        gulp.series('copy_css_wc_layout'))
+	gulp.watch(localWCAssets + 'woocommerce.css',               gulp.series('copy_css_wc_default'))
+	gulp.watch(localWCBuild + 'wc-blocks-style.css',            gulp.series('copy_css_wc_blocks'))
+	gulp.watch(localWCBuild + 'wc-blocks-vendors-style.css',    gulp.series('copy_css_wc_vendors'))
+	gulp.watch(localWCLibrary + 'style.min.css',                gulp.series('copy_css_wc_library'))
 })
 
-gulp.task('default', gulp.series(
-	'watch'
-))
+gulp.task('default', gulp.series('watch'))
