60fe7a40d3a2a3f231045cad615b99ab84fce9d5
dfox <dfox@foxartbox.com>
2023-07-15 16:31:47 +0300

    New buildings. ref

--- wp-content/themes/broker2022/template-parts/new_buildings.php
+++ wp-content/themes/broker2022/template-parts/new_buildings.php
@@ -1,12 +1,10 @@
 <?php // link to images folder
-$i      = '/wp-content/themes/broker2022/i/hero';
-$dumm   = '/wp-content/themes/broker2022/i/dumm.png';
-?>
+$i = '/wp-content/themes/broker2022/i/hero'; ?>
 
 <div class="hero_block_slider">
 	<div class="owl-carousel js-owl-carousel-auto-height">
 		<div class="hero_block_slide">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/20.jpg" alt="" loading="lazy">
+			<span class="hero_block_slide__img js-img-scroll" data-src="<?= $i; ?>/20.jpg"></span>
 
 			<div class="hero_block_slide__content">
 				<div class="hero_block_slide__title">Новостройки</div>
@@ -18,7 +16,7 @@ $dumm   = '/wp-content/themes/broker2022/i/dumm.png';
 		</div>
 
 		<div class="hero_block_slide hero_block_slide--light">
-			<img class="hero_block_slide__img js-img-scroll" src="<?= $dumm; ?>" data-src="<?= $i; ?>/23.jpg" alt="" loading="lazy">
+			<span class="hero_block_slide__img js-img-scroll" data-src="<?= $i; ?>/23.jpg"></span>
 
 			<div class="hero_block_slide__content">
 				<?= do_shortcode('[contact-form-7 id="7237"]'); ?>
