e41e710ef10b5ab43386e89c9520a8616b259b37
dfox <dfox@foxartbox.com>
2023-08-29 12:18:18 +0300

    AMO updates

--- send_amo.php
+++ send_amo.php
@@ -7,16 +7,18 @@
 	<?php require_once 'wp-load.php';
 	require __DIR__ . '/vendor/autoload.php';
 
-//	if (isset($_POST['user-phone'])) {
-		try {
+	if (isset($_POST['user-phone'])) {
+		/*try {
 			$subdomain = '1amotopbrokermoscow';
 			$login     = '1@topbroker.Moscow';
 			$apikey    = '543b7a20dd7ff2cb50f46407b8cde4a2972368ee';
 
 			$amo            = new \AmoCRM\Client($subdomain, $login, $apikey);
-			$lead           = $amo->lead;
-			$lead['name']   = $_POST['product_name'];
-			$id             = $lead->apiAdd();
+			$lead = $amo->lead;
+			$lead['name'] = $_POST['product_name'];
+			$lead['pipeline_id'] = 4507276; 
+			$id = $lead->apiAdd();
+			
 
 			$contact            = $amo->contact;
 			$contact['name']    = isset($_POST['user-name']) ? $_POST['user-name'] : 'Не указано';
@@ -31,9 +33,16 @@
 				[$_POST['page-url'], 'PRIV']
 			]);
 			$id = $contact->apiAdd();
-
-			// test all fileds
-//			print_r($amo->account->apiCurrent());
+			
+					}
+		catch (\AmoCRM\Exception $e) {
+			printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
+		}*/
+			/*$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			if ($statusCode != 200) {
+				die('Ошибка при отправке данных: ' . $statusCode);
+			}*/
+			//print_r($amo->pipelines->apiList());
 
 			$url = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 
@@ -55,79 +64,5 @@
 			else {
 				echo "Ошибка отправки";
 			}
-		}
-		catch (\AmoCRM\Exception $e) {
-			printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
-//		}
 	} ?>
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>Ваша заявка успешно отправлена</title>
-
-	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans" rel="stylesheet">
-
-	<style>
-		body,
-		html {
-			background-color: #fff;
-			color: #636b6f;
-			font-family: 'IBM Plex Sans', sans-serif;
-			font-weight: 100;
-			height: 100vh;
-			margin: 0;
-		}
-
-		.back-link {
-			text-decoration: none;
-			border-bottom: 1px dotted;
-		}
-
-		.content {
-			text-align: center;
-		}
-
-		.flex-center {
-			align-items: center;
-			display: flex;
-			justify-content: center;
-		}
-
-		.full-height {
-			height: 100vh;
-		}
-
-		.position-ref {
-			position: relative;
-		}
-
-		.success-message {
-			font-size: 33px;
-			font-weight: 500;
-			margin-bottom: 20px;
-		}
-
-		.title {
-			font-size: 20px;
-			padding: 20px;
-		}
-	</style>
-</head>
-
-<body>
-	<div class="flex-center position-ref full-height">
-		<div class="content">
-			<div class="title">
-				<p class="success-message">Спасибо!</p>
-
-				<p>Ваша заявка успешно отправлена.</p>
-
-				<?php if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']) { ?>
-					<br><br><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" class="back-link">Вернуться назад</a>
-				<?php } ?>
-			</div>
-		</div>
-	</div>
-</body>
 </html>
